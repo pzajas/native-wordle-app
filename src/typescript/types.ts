6f98e1ad3f31ec2ac98d60fb62cd59fc5023e55d
pzajas <zajas.piotr@gmail.com>
2023-07-05 20:07:01 +0200

    Install axios and fetch api data.

---
+++ src/typescript/types.ts
@@ -0,0 +1,10 @@
+export type UserInputProps = {
+  index: number
+  checkWord: boolean
+  setCheckWord: (value: boolean) => void
+  randomWord: string
+}
+
+export type GameProps = {
+  randomWord: string
+}
