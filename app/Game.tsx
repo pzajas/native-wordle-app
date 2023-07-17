c54d7bfd6881f05727ca25c047d2b6245f39ff9d
pzajas <zajas.piotr@gmail.com>
2023-07-17 23:32:34 +0200

    Change the input handling logic.

--- app/Game.tsx
+++ app/Game.tsx
@@ -1,46 +1,75 @@
-import { FunctionComponent, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
-import { GameProps } from '../src/typescript/types'
+import React, { useRef, useEffect, useState } from 'react'
+import { View, TextInput, Button } from 'react-native'
 
-import UserInput from './UserInput'
+export default function App() {
+  const [count, setCount] = useState(1)
+  const [tries, setTries] = useState(1)
 
-const Game: FunctionComponent<GameProps> = ({
-  randomWord,
-}) => {
-  const [checkWord, setCheckWord] = useState(false)
-  const [guessWord, setGuessWord] = useState<string[]>([])
+  const refs = Array(25)
+    .fill(0)
+    .map(() => useRef())
 
-  console.log(randomWord)
-
-  const renderUserInputs = () => {
-    const inputs = Array.from({ length: 5 }, (_, i) => (
-      <UserInput
-        key={i}
-        index={i}
-        checkWord={checkWord}
-        setCheckWord={setCheckWord}
-        randomWord={randomWord}
-        guessWord={guessWord}
-        setGuessWord={setGuessWord}
-      />
-    ))
-
-    return inputs
+  const handleTextChange = (index, text, e) => {
+    if (
+      text.length >= 1 &&
+      index < refs.length - 1 &&
+      count !== 5
+    ) {
+      refs[index + 1].current.focus()
+      setCount((prevState) => prevState + 1)
+    } else if (
+      count % 5 === 0 &&
+      e &&
+      e.nativeEvent.key === 'Enter'
+    ) {
+      console.log(index)
+    }
   }
 
-  return (
-    <View style={styles.container}>
-      {renderUserInputs()}
-    </View>
-  )
+  const onSubmit = (e) => {
+    if (count === 5 && e) {
+      console.log('ppp')
+    }
+  }
+
+  useEffect(() => {
+    refs[0].current.focus()
+  }, [])
+
+  const inputs = refs.map((ref, index) => (
+    <TextInput
+      maxLength={1}
+      key={index}
+      ref={ref}
+      style={{
+        backgroundColor: 'grey',
+        width: 40,
+        height: 40,
+      }}
+      onSubmitEditing={onSubmit}
+      onChangeText={(text) => handleTextChange(index, text)}
+      onKeyPress={(e) =>
+        handleTextChange(index, ref.current?.value, e)
+      }
+    />
+  ))
+
+  const rows = []
+  let row = []
+  for (let i = 0; i < inputs.length; i++) {
+    row.push(inputs[i])
+    if ((i + 1) % 5 === 0 || i === inputs.length - 1) {
+      rows.push(
+        <View
+          key={i}
+          style={{ flexDirection: 'row', gap: 10 }}
+        >
+          {row}
+        </View>
+      )
+      row = []
+    }
+  }
+
+  return <View style={{ gap: 10 }}>{rows}</View>
 }
-
-export default Game
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    gap: 5,
-  },
-})
